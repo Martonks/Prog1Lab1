606b78eadd3a5235ca59c34c463d67d8e1616606
Martonks <martycal@outlook.com>
2020-01-31 13:28:15 -0500

    Update

---
+++ MetersToFeet.java
@@ -0,0 +1,13 @@
+public class MetersToFeet {
+
+    public static void main(String[] args) {
+    	double meters = 1;
+    	double feet;
+
+    feet = meters * 3.28084;
+
+    	System.out.println("1 meter is equal to " + feet + " ft");
+         
+    }
+
+}
