bd2ce2fc5ea8da56293a639e4ae9078171397363
Martonks <martycal@outlook.com>
2020-01-31 13:24:47 -0500

    Update

---
+++ MyAgeIs.java
@@ -0,0 +1,14 @@
+public class MyAgeIs {
+
+    public static void main(String[] args) {
+    	int yearOfBirth = 2002;
+    	int currentYear = 2020;
+    	double age;
+
+    	age = currentYear - yearOfBirth;
+
+    	System.out.println("I am " + age + " years old");
+         
+    }
+
+}
