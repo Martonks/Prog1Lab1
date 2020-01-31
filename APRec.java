56680e694bb10a7639d8cd149ea1e65eacadf47a
Martonks <martycal@outlook.com>
2020-01-31 13:48:18 -0500

    Update

---
+++ APRec.java
@@ -0,0 +1,15 @@
+public class APRec {
+
+    public static void main(String[] args) {
+    	double x = 5;
+    	double y = 4;
+    	double area;
+    	double perimeter;
+
+    	area = x * y;
+    	perimeter = 2*(x + y);
+
+    	System.out.println("Given that the base is " + x + " cm and the height is " + y + " cm. The area would be " + area + " cm and the perimeter would be " + perimeter + " cm");
+    }
+
+}
