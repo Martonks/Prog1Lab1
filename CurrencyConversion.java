e6a905bb6f8d3669a2de59ce72fbc2971c8f5d5d
Martonks <martycal@outlook.com>
2020-01-31 13:36:47 -0500

    Lab

---
+++ CurrencyConversion.java
@@ -0,0 +1,15 @@
+public class CurrencyConversion {
+
+    public static void main(String[] args) {
+    	double usd;
+    	double eur;
+    	double cad = 1;
+
+    	usd = cad * 0.75;
+    	eur = cad * 0.68;
+
+    	System.out.println("1 dollar CAD is equal to " + usd + " US dollars and " + eur + " euros");
+
+    }
+
+}
