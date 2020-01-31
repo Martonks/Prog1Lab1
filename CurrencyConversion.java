9da53dd173f3f029293eb439254be8eaaba32cc2
Martonks <martycal@outlook.com>
2020-01-31 13:39:17 -0500

    Update

--- CurrencyConversion.java
+++ CurrencyConversion.java
@@ -1,6 +1,7 @@
 public class CurrencyConversion {
 
     public static void main(String[] args) {
+    	//Prints out the conversion of currency between CAD, USD and EUR
     	double usd;
     	double eur;
     	double cad = 1;
@@ -9,6 +10,7 @@ public class CurrencyConversion {
     	eur = cad * 0.68;
 
     	System.out.println("1 dollar CAD is equal to " + usd + " US dollars and " + eur + " euros");
+
 
     }
 
